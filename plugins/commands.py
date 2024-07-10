d0b6e88a5cc3461912c7f0fb976b6fe082665503
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-07-10 22:52:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -596,7 +596,7 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"@VJ_Bots  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
+        f_caption = f"@TMR_update_channel  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
