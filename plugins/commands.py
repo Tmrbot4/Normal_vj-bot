7fda7e42ccce5e98001f4ffefe1ba62a392556a6
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-07-11 15:48:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1451,7 +1451,7 @@ async def give_premium_cmd_handler(client, message):
             await message.reply_text("Premium access added to the user.")            
             await client.send_message(
                 chat_id=user_id,
-                text=f"<b>ʜɪɪ Bʀᴏ / sɪs,\n\nᴘʀᴇᴍɪᴜᴍ ᴀᴅᴅᴇᴅ ᴛᴏ ʏᴏᴜʀ ᴀᴄᴄᴏᴜɴᴛ 😀</b>",                
+                text=f"<b>ʜɪɪ Bʀᴏ / sɪs,\n\nᴘʀᴇᴍɪᴜᴍ ᴀᴅᴅᴇᴅ ᴛᴏ ʏᴏᴜʀ ᴀᴄᴄᴏᴜɴᴛ ꜰᴏʀ {time} ᴇɴᴊᴏʏ 😀</b>",                
             )
         else:
             await message.reply_text("Invalid time format. Please use '1day for days', '1hour for hours', or '1min for minutes', or '1month for months' or '1year for year'")
